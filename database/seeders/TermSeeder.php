22aad60a77538b7b98ef6af39535624bf54b16ca
m03zam117 <118348230+m03zam117@users.noreply.github.com>
2022-11-24 18:14:19 +1100

    Users,Subjects,Terms and Database Seeders

--- database/seeders/TermSeeder.php
+++ database/seeders/TermSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
  
-class UserSeeder extends Seeder
+class TermSeeder extends Seeder
 {
     /**
      * Run the database seeders.
@@ -16,102 +16,146 @@ class UserSeeder extends Seeder
 	 
        public function run()
     {
-        User::table('terms')->insert([
+        Term::table('terms')->insert([
 		'year' => '2021',
 		'month' => 'January',
-        ]
-		[
+		'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+		]);
+
+		Term::table('terms')->insert([
 		'year' => '2021',
 		'month' => 'February',
-        ]
-		[
+		'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+		Term::table('terms')->insert([
 		'year' => '2021',
 		'month' => 'March',
-        ]
-		[
+		'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+		Term::table('terms')->insert([
 		'year' => '2021',
 		'month' => 'April',
-        ]
-		[
+		'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+		Term::table('terms')->insert([
 		'year' => '2021',
 		'month' => 'May',
-        ]
-		[
+		'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+		Term::table('terms')->insert([
 		'year' => '2021',
 		'month' => 'June',
-        ]
-		[
+		'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+		Term::table('terms')->insert([
 		'year' => '2021',
 		'month' => 'July',
-        ]
-		[
+		'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+		Term::table('terms')->insert([
 		'year' => '2021',
 		'month' => 'August',
-        ]
-		[
+        ]);
+
+		Term::table('terms')->insert([
 		'year' => '2021',
 		'month' => 'September',
-        ]
-		[
+        ]);
+
+		Term::table('terms')->insert([
 		'year' => '2021',
 		'month' => 'October',
-        ]
-		[
+		'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+		Term::table('terms')->insert([
 		'year' => '2021',
 		'month' => 'November',
-        ]
-		[
+		'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+		Term::table('terms')->insert([
 		'year' => '2021',
 		'month' => 'December',
-        ]
-		[
+		'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+		Term::table('terms')->insert([
 		'year' => '2022',
 		'month' => 'January',
-        ]
-		[
+		'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+		Term::table('terms')->insert([
 		'year' => '2022',
 		'month' => 'February',
-        ]
-		[
+		'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+		Term::table('terms')->insert([
 		'year' => '2022',
 		'month' => 'March',
-        ]
-		[
+		'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+		Term::table('terms')->insert([
 		'year' => '2022',
 		'month' => 'April',
-        ]
-		[
+		'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+		Term::table('terms')->insert([
 		'year' => '2022',
 		'month' => 'May',
-        ]
-		[
+		'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+		Term::table('terms')->insert([
 		'year' => '2022',
 		'month' => 'June',
-        ]
-		[
+		'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+		Term::table('terms')->insert([
 		'year' => '2022',
 		'month' => 'July',
-        ]
-		[
+		'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+		Term::table('terms')->insert([
 		'year' => '2022',
 		'month' => 'August',
-        ]
-		[
+		'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+		Term::table('terms')->insert([
 		'year' => '2022',
 		'month' => 'September',
-        ]
-		[
+		'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+		Term::table('terms')->insert([
 		'year' => '2022',
 		'month' => 'October',
-        ]
-		[
+		'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+		Term::table('terms')->insert([
 		'year' => '2022',
 		'month' => 'November',
-        ]
-		[
+		'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+		Term::table('terms')->insert([
 		'year' => '2022',
 		'month' => 'December',
-        ]
-		);
+		'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -22,79 +22,90 @@ class UserSeeder extends Seeder
 			'maxLoad' => '1.0',
             'email' => Str::random(10)'@gmail.com',
 			'password' => Hash::make('password'),
-        ]
-		[
+		]);
+
+		User::table('users')->insert([[
 			'firstName' => 'Beech',
 			'maxLoad' => '1.0',
             'email' => Str::random(10).'@gmail.com',
 			'password' => Hash::make('password'),
-		]
-		[
+		]);
+		
+		User::table('users')->insert([
 			'firstName' => 'Cypress',
 			'maxLoad' => '1.0',
             'email' => Str::random(10).'@gmail.com',
 			'password' => Hash::make('password'),
-		]
-		[
+		]);
+
+		User::table('users')->insert([
 			'firstName' => 'Douglas',
 			'maxLoad' => '0.8',
             'email' => Str::random(10).'@gmail.com',
 			'password' => Hash::make('password'),
-		]
-		[
+		]);
+
+		User::table('users')->insert([
 			'firstName' => 'Eucalypt',
 			'maxLoad' => '0.4',
             'email' => Str::random(10).'@gmail.com',
 			'password' => Hash::make('password'),
-		]
-		[
+		]);
+		
+		User::table('users')->insert([
 			'firstName' => 'Flame',
 			'maxLoad' => '1.0',
             'email' => random(10).'@gmail.com',
 			'password' => Hash::make('password'),
-		]
-		[
+		]);
+		
+		User::table('users')->insert([
 			'firstName' => 'Guava',
 			'maxLoad' => '0.6',
             'email' => random(10).'@gmail.com',
 			'password' => Hash::make('password'),
-		]
-		[
+		]);
+
+		User::table('users')->insert([
 			'firstName' => 'Hickory',
 			'maxLoad' => '0.8',
             'email' => random(10).'@gmail.com',
 			'password' => Hash::make('password'),
-		]
-		[
+		]);
+
+		User::table('users')->insert([
 			'firstName' => 'Ironbark',
 			'maxLoad' => '1.0',
             'email' => random(10).'@gmail.com',
 			'password' => Hash::make('password'),
-		]
-		[
+		]);
+
+		User::table('users')->insert([
 			'firstName' => 'Jacaranda',
 			'maxLoad' => '0.4',
             'email' => random(10).'@gmail.com',
 			'password' => Hash::make('password'),
-		]
-		[
+		]);
+
+		User::table('users')->insert([
 			'firstName' => 'Karri',
 			'maxLoad' => '1.0',
             'email' => random(10).'@gmail.com',
 			'password' => Hash::make('password'),
-		]
-		[
+		]);
+
+		User::table('users')->insert([
 			'firstName' => 'Laurel',
 			'maxLoad' => '1.0',
             'email' => random(10).'@gmail.com',
 			'password' => Hash::make('password'),
-		]
-		[
+		]);
+
+		User::table('users')->insert([
 			'firstName' => 'Mapel',
 			'maxLoad' => '1.0',
             'email' => random(10).'@gmail.com',
 			'password' => Hash::make('password'),
-		]
-		);
+		]);
     }
 }
