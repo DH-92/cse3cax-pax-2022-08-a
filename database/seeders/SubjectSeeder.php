d5216adf37ed1975fdf82b04c86840bc53eccf6a
m03zam117 <118348230+m03zam117@users.noreply.github.com>
2022-11-27 22:03:49 +1100

    Users,Subjects,Terms and Database Seeders

--- database/seeders/SubjectSeeder.php
+++ database/seeders/SubjectSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
  
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Subject;
+use Illuminate\Support\Facades\DB;
 
  
 class SubjectSeeder extends Seeder
@@ -16,192 +16,192 @@ class SubjectSeeder extends Seeder
 	 
        public function run()
     {
-        Subject::table('subjects')->insert([
+        DB::table('DBs')->insert([
 			'code' => 'CSE1ITX',
 			'name'  => 'Information Technology Fundamentals',
 		]);
 
-		Subject::table('subjects')->insert([
+		DB::table('DBs')->insert([
 			'code' => 'CSE1PGX',
 			'name'  => 'Programming Environments',
         ]);
 
-		Subject::table('subjects')->insert([
+		DB::table('DBs')->insert([
 			'code' => 'CSE1CFX',
 			'name'  => 'Cloud Foundations',
         ]);
 
-		Subject::table('subjects')->insert([
+		DB::table('DBs')->insert([
 			'code' => 'CSE1OFX',
 			'name'  => 'Object Oriented Programming Fundamentals',
         ]);
 
-		Subject::table('subjects')->insert([
+		DB::table('DBs')->insert([
 			'code' => 'CSE1ISX',
 			'name'  => 'Information Systems',
         ]);
 
-		Subject::table('subjects')->insert([
+		DB::table('DBs')->insert([
 			'code' => 'CSE2NFX',
 			'name'  => 'Network Engineering Fundamentals',
         ]);
 
-		Subject::table('subjects')->insert([
+		DB::table('DBs')->insert([
 			'code' => 'CSE2DCX',
 			'name'  => 'Database Fundamentals on the Cloud',
         ]);
 
-		Subject::table('subjects')->insert([
+		DB::table('DBs')->insert([
 			'code' => 'CSE1SPX',
 			'name'  => 'Sustainability Practices',
         ]);
 
-		Subject::table('subjects')->insert([
+		DB::table('DBs')->insert([
 			'code' => 'CSE1IOX',
 			'name'  => 'Intermediate Object Oriented Programming',
         ]);
 
-		Subject::table('subjects')->insert([
+		DB::table('DBs')->insert([
 			'code' => 'CSE2ICX',
 			'name'  => 'Internet Client Engineering',
         ]);
 
-		Subject::table('subjects')->insert([
+		DB::table('DBs')->insert([
 			'code' => 'CSE2CNX',
 			'name'  => 'Computer Networks',
         ]);
 
-		Subject::table('subjects')->insert([
+		DB::table('DBs')->insert([
 			'code' => 'CSE2SDX',
 			'name'  => 'Information Systems Development',
         ]);
 
-		Subject::table('subjects')->insert([
+		DB::table('DBs')->insert([
 			'code' => 'BUS2PMX',
 			'name'  => 'Project Management',
         ]);
 
-		Subject::table('subjects')->insert([
+		DB::table('DBs')->insert([
 			'code' => 'CSE2VVX',
 			'name'  => 'Virtualisation for the Cloud',
         ]);
 
-		Subject::table('subjects')->insert([
+		DB::table('DBs')->insert([
 			'code' => 'MAT2DMX',
 			'name'  => 'Discrete Mathematics for Computer Science',
         ]);
 
-		Subject::table('subjects')->insert([
+		DB::table('DBs')->insert([
 			'code' => 'CSE2MAX',
 			'name'  => 'Mobile Application Development',
         ]);
 
-		Subject::table('subjects')->insert([
+		DB::table('DBs')->insert([
 			'code' => 'CSE2OSX',
 			'name'  => 'Operating Systems',
         ]);
 
-		Subject::table('subjects')->insert([
+		DB::table('DBs')->insert([
 			'code' => 'CSE2ADX',
 			'name'  => 'Application Development in the Cloud',
         ]);
 
-		Subject::table('subjects')->insert([
+		DB::table('DBs')->insert([
 			'code' => 'CSE2CPX',
 			'name'  => 'Managing Projects in the Cloud',
         ]);
 
-		Subject::table('subjects')->insert([
+		DB::table('DBs')->insert([
 			'code' => 'CSE2MLX',
 			'name'  => 'Machine Learning',
         ]);
 
-		Subject::table('subjects')->insert([
+		DB::table('DBs')->insert([
 			'code' => 'CSE2SAX',
 			'name'  => 'Operating Systems Administration',
         ]);
 
-		Subject::table('subjects')->insert([
+		DB::table('DBs')->insert([
 			'code' => 'CSE2ANX',
 			'name'  => 'Advanced Computer Networks',
         ]);
 
-		Subject::table('subjects')->insert([
+		DB::table('DBs')->insert([
 			'code' => 'CSE2WDX',
 			'name'  => 'Web Development',
         ]);
 
-		Subject::table('subjects')->insert([
+		DB::table('DBs')->insert([
 			'code' => 'CSE3BGX',
 			'name'  => 'Big Data Management on the Cloud (Elective)',
         ]);
 
-		Subject::table('subjects')->insert([
+		DB::table('DBs')->insert([
 			'code' => 'CSE3CIX',
 			'name'  => 'Computational Intelligence for Data Analysis (Elective)',
         ]);
 
-		Subject::table('subjects')->insert([
+		DB::table('DBs')->insert([
 			'code' => 'CSE3CSX',
 			'name'  => 'Cybersecurity Fundamentals (Elective)',
         ]);
 
-		Subject::table('subjects')->insert([
+		DB::table('DBs')->insert([
 			'code' => 'CSE3NWX',
 			'name'  => 'Networks, Systems and Web Security (Elective)',
         ]);
 
-		Subject::table('subjects')->insert([
+		DB::table('DBs')->insert([
 			'code' => 'CSE3OTX',
 			'name'  => 'Internet of Things (Elective)',
         ]);
 
-		Subject::table('subjects')->insert([
+		DB::table('DBs')->insert([
 			'code' => 'CSE3WSX',
 			'name'  => 'Wireless Network Engineering (Elective)',
         ]);
 
-		Subject::table('subjects')->insert([
+		DB::table('DBs')->insert([
 			'code' => 'CSE3PAX',
 			'name'  => 'Industry Project 3A',
         ]);
 
-		Subject::table('subjects')->insert([
+		DB::table('DBs')->insert([
 			'code' => 'CSE3PBX',
 			'name'  => 'Industry Project 3B',
         ]);
 
-		Subject::table('subjects')->insert([
+		DB::table('DBs')->insert([
 			'code' => 'CSE3PEX',
 			'name'  => 'Profesional Environment',
         ]);
 
-		Subject::table('subjects')->insert([
+		DB::table('DBs')->insert([
 			'code' => 'CSE3ACX',
 			'name'  => 'Architecting on the Cloud',
         ]);
 
-		Subject::table('subjects')->insert([
+		DB::table('DBs')->insert([
 			'code' => 'CSE3SOX',
 			'name'  => 'System Operations on the Cloud',
         ]);
 
-		Subject::table('subjects')->insert([
+		DB::table('DBs')->insert([
 			'code' => 'CSE3BDX',
 			'name'  => 'Big Data on the Cloud',
         ]);
 
-		Subject::table('subjects')->insert([
+		DB::table('DBs')->insert([
 			'code' => 'CSE3PCX',
 			'name'  => 'Private Cloud Solutions',
         ]);
 
-		Subject::table('subjects')->insert([
+		DB::table('DBs')->insert([
 			'code' => 'CSE3CAX',
 			'name'  => 'Industry Project for Cloud Technology 3A',
         ]);
 
-		Subject::table('subjects')->insert([
+		DB::table('DBs')->insert([
 			'code' => 'CSE3CBX',
 			'name'  => 'Industry Project for Cloud Technology 3B',
         ]);
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
  
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\User;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
  
@@ -17,91 +17,91 @@ class UserSeeder extends Seeder
 	 
     public function run()
     {
-        User::table('users')->insert([
+    DB::table(DBs')->insert([
 			'firstName' => 'Acacia',
 			'maxLoad' => '1.0',
             'email' => Str::random(10)'@gmail.com',
 			'password' => Hash::make('password'),
 		]);
 
-		User::table('users')->insert([
+	DB::table(DBs')->insert([
 			'firstName' => 'Beech',
 			'maxLoad' => '1.0',
             'email' => Str::random(10).'@gmail.com',
 			'password' => Hash::make('password'),
 		]);
 		
-		User::table('users')->insert([
+	DB::table(DBs')->insert([
 			'firstName' => 'Cypress',
 			'maxLoad' => '1.0',
             'email' => Str::random(10).'@gmail.com',
 			'password' => Hash::make('password'),
 		]);
 
-		User::table('users')->insert([
+	DB::table(DBs')->insert([
 			'firstName' => 'Douglas',
 			'maxLoad' => '0.8',
             'email' => Str::random(10).'@gmail.com',
 			'password' => Hash::make('password'),
 		]);
 
-		User::table('users')->insert([
+	DB::table(DBs')->insert([
 			'firstName' => 'Eucalypt',
 			'maxLoad' => '0.4',
             'email' => Str::random(10).'@gmail.com',
 			'password' => Hash::make('password'),
 		]);
 		
-		User::table('users')->insert([
+	DB::table(DBs')->insert([
 			'firstName' => 'Flame',
 			'maxLoad' => '1.0',
-            'email' => random(10).'@gmail.com',
+            'email' => Str::random(10).'@gmail.com',
 			'password' => Hash::make('password'),
 		]);
 		
-		User::table('users')->insert([
+	DB::table(DBs')->insert([
 			'firstName' => 'Guava',
 			'maxLoad' => '0.6',
             'email' => random(10).'@gmail.com',
 			'password' => Hash::make('password'),
 		]);
 
-		User::table('users')->insert([
+	DB::table(DBs')->insert([
 			'firstName' => 'Hickory',
 			'maxLoad' => '0.8',
             'email' => random(10).'@gmail.com',
 			'password' => Hash::make('password'),
 		]);
 
-		User::table('users')->insert([
+	DB::table(DBs')->insert([
 			'firstName' => 'Ironbark',
 			'maxLoad' => '1.0',
             'email' => random(10).'@gmail.com',
 			'password' => Hash::make('password'),
 		]);
 
-		User::table('users')->insert([
+	DB::table(DBs')->insert([
 			'firstName' => 'Jacaranda',
 			'maxLoad' => '0.4',
             'email' => random(10).'@gmail.com',
 			'password' => Hash::make('password'),
 		]);
 
-		User::table('users')->insert([
+	DB::table(DBs')->insert([
 			'firstName' => 'Karri',
 			'maxLoad' => '1.0',
             'email' => random(10).'@gmail.com',
 			'password' => Hash::make('password'),
 		]);
 
-		User::table('users')->insert([
+	DB::table(DBs')->insert([
 			'firstName' => 'Laurel',
 			'maxLoad' => '1.0',
             'email' => random(10).'@gmail.com',
 			'password' => Hash::make('password'),
 		]);
 
-		User::table('users')->insert([
+	DB::table(DBs')->insert([
 			'firstName' => 'Mapel',
 			'maxLoad' => '1.0',
             'email' => random(10).'@gmail.com',
