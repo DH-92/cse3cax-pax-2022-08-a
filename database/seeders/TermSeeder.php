617a78ef9e145e769c791048bcfdc836c324b65d
m03zam117 <118348230+m03zam117@users.noreply.github.com>
2022-11-27 21:39:31 +1100

    Update TermSeeder.php

--- database/seeders/TermSeeder.php
+++ database/seeders/TermSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
  
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Term;
 use Carbon\Carbon;
  
 class TermSeeder extends Seeder
