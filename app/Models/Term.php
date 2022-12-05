c6e1084515f6956ef401cef14c8037fb9556316d
Keenan Anderson <keenananderson@hotmail.com>
2022-12-05 18:08:23 +1100

    add model relationships, update seeders/terms migration

--- app/Models/Term.php
+++ app/Models/Term.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Term extends Model
 {
     use HasFactory;
+
+    public function subject()
+    {
+        return $this->hasMany(SubjectInstance::class);
+    }
 }
