2ae622ac3bb0424a26ec335dac64375f46467a54
m03zam117 <118348230+m03zam117@users.noreply.github.com>
2022-12-16 01:58:54 +0000

    Dashboards

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -12,7 +12,13 @@
 <div style="background-color:#08508a;"class="col-sm-8">
       <div class="well">
         <h4 style="color:white;"> Subjects</h4>
-        <a href="admin/subjects">List Subjects</a>
+        <a "background-color:white;" href="admin/subjects">List Subjects</a>
+      </div>
+</div>
+</div>
+<div class="row">
+<div style="background-color:white;"class="col-sm-4">
+      <div class="well">
       </div>
 </div>
 </div>
@@ -20,7 +26,7 @@
         <div  style="background-color:#08508a;"class="col-sm-8">
           <div class="well">
             <h4 style="color:white;"> Users</h4>
-            <a href="admin/users">List Users</a>
+            <a style="background-color:white;" href="admin/users">List Users</a>
           </div>
         </div>
 </div>
