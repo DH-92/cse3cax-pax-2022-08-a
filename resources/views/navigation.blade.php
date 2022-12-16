63c5918a2ffc3056534d2aacf713e72121f365d0
matt <pricem1990@gmail.com>
2022-12-16 16:38:06 +1000

    ca2-85: NavBar - change userType displays to use new Auth mechanism

--- resources/views/navigation.blade.php
+++ resources/views/navigation.blade.php
@@ -1,16 +1,19 @@
+@php
+    $userType = Session::get('user')->userType;
+@endphp
 <div class="col-auto col-md-3 col-xl-2 col-sm-3 px-0 bg-didasko">
     <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
         <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-decoration-none link-light">
             <span class="fs-5 d-none d-sm-inline align-items-center"><img class="mw-100" src="{{ URL::asset('images/didasko-logo.jpg') }}" alt="Didasko Logo" /></span>
         </a>
         <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start " id="menu">
-        @if(Session::get('permission') == 1)
+        @if($userType == 0)
             <li>
-                <a href="#" class="nav-link px-0 align-middle link-light">
+                <a href="/lecturer/schedule" class="nav-link px-0 align-middle link-light">
                     <i class="fa-regular fa-calendar-days"></i> <span class="ms-1 d-none d-sm-inline">My Schedule</span>
                 </a>
             </li>
-        @elseif(Session::get('permission') == 2)
+        @elseif($userType == 1)
             <li>
                 <a href="#submenu1" data-bs-toggle="collapse" class="nav-link px-0 align-middle link-light">
                     <i class="fa-solid fa-pen-to-square"></i> <span class="ms-1 d-none d-sm-inline">Manage</span></a>
@@ -23,7 +26,7 @@
                     </li>
                 </ul>
             </li>
-        @elseif(Session::get('permission') == 3)
+        @elseif($userType == 2)
             <li>
                 <a href="#submenu1" data-bs-toggle="collapse" class="nav-link px-0 align-middle link-light">
                     <i class="fa-solid fa-pen-to-square"></i> <span class="ms-1 d-none d-sm-inline">Administration</span></a>
