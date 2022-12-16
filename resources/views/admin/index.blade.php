28509163d933170f5f3f866ad96d0e8497d22a31
matt <pricem1990@gmail.com>
2022-12-16 17:26:28 +1000

    ca2-85: Admin Dashboard - change links to cards/icons

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,24 +5,35 @@
 @include('header')
 @include('navigation')
 {{--Actual content starts here--}}
-
-<div class="container">
-<div class="row">
-<div style="background-color:#08508a;"class="col-sm-8">
-      <div class="well">
-        <h4 style="color:white;"> Subjects</h4>
-        <a style = "background-color:white;" href="admin/subjects">List Subjects</a>
-      </div>
-</div>
-</div>
-<div class="row">
-        <div  style="background-color:#08508a;"class="col-sm-8">
-          <div class="well">
-            <h4 style="color:white;"> Users</h4>
-            <a style="background-color:white;" href="admin/users">List Users</a>
-          </div>
+<div class="container d-flex justify-content-start">
+    <a href="/admin/subjects" class="nav-link px-0 link-primary">
+        <div id="card" class="d-flex justify-content-center w-100 border border-2 border-primary m-3">
+            <div id="card-content">
+                <div id="card-title">
+                    <div class="col pb-3">
+                        <span class="h2">Subjects</span>
+                    </div>
+                    <div class="col d-flex justify-content-center">
+                     <i class="fa-solid fa-book-open fa-5x"></i>
+                    </div>
+                </div>
+            </div>
         </div>
-</div>
+    </a>
+    <a href="/admin/users" class="nav-link link-primary">
+        <div id="card" class="d-flex justify-content-center w-100 border border-2 border-primary ms-5 m-3">
+            <div id="card-content">
+                <div id="card-title">
+                    <div class="col pb-3">
+                        <span class="h2">Users</span>
+                    </div>
+                    <div class="col d-flex justify-content-center">
+                        <i class="fa-solid fa-users fa-5x"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </a>
 </div>
 {{--close off any opening tags made in header/navigation--}}
 @include('footer')
