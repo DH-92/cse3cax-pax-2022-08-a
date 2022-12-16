845f67a705863e8807c13b0730d53240bff190c4
m03zam117 <118348230+m03zam117@users.noreply.github.com>
2022-12-16 00:51:02 +0000

    Administration dashboards

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -6,8 +6,19 @@
 @include('navigation')
 
 {{--Actual content starts here--}}
-<a href="admin/subjects">List Subjects</a>
-<a href="admin/users">List Users</a>
-
+<br>
+    
+    <div class="col-sm-9">
+      <div class="well">
+        <h4>Subjects</h4>
+        <a href="admin/subjects">List Subjects</a>
+      </div>
+      <div class="row">
+        <div class="col-sm-9">
+          <div class="well">
+            <h4>Users</h4>
+            <a href="admin/users">List Users</a>
+          </div>
+        </div>
 {{--close off any opening tags made in header/navigation--}}
 @include('footer')
