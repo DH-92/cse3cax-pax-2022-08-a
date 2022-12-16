8e8170c3fc8d5ce969df925642acee6d14b2df17
m03zam117 <118348230+m03zam117@users.noreply.github.com>
2022-12-16 01:34:54 +0000

    Dashboards

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -4,22 +4,25 @@
 
 @include('header')
 @include('navigation')
-
 {{--Actual content starts here--}}
+
 <br>
-    
-<div style="background-color:#0e6ea1;"class="col-sm-9">
+<div class="row">
+<div style="background-color:#08508a;"class="col-sm-9">
       <div class="well">
         <h4 style="color:white;"> Subjects</h4>
         <a href="admin/subjects">List Subjects</a>
       </div>
-</br>     
-<br>
-        <div  style="background-color:#0e6ea1;"class="col-sm-9">
+</div>
+</div>
+
+<div class="row">
+        <div  style="background-color:#08508a;"class="col-sm-9">
           <div class="well">
             <h4 style="color:white;"> Users</h4>
             <a href="admin/users">List Users</a>
           </div>
         </div>
+</div>
 {{--close off any opening tags made in header/navigation--}}
 @include('footer')
