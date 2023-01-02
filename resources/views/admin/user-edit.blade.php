c82ac885366dfde7b54bde170cbee6667a3d8527
matt <pricem1990@gmail.com>
2023-01-02 16:54:58 +1000

    ca2-86: Add/Edit User - Show/Hide lecturer-only options

--- resources/views/admin/user-edit.blade.php
+++ resources/views/admin/user-edit.blade.php
@@ -45,7 +45,6 @@
             </div>
             <div class="col-4">
                 <select id="employmentType" name="employmentType" class="selectpicker w-100 border rounded border-primary">
-                    <option value="Other">Other</option>
                     @foreach(UserController::getEmploymentTypes() as $type)
                         <option value="{{ $type }}" @if (($user->employmentType ?? "Other") == $type) selected @endif>{{ $type }}</option>
                     @endforeach
@@ -62,31 +61,33 @@
                 </select>
             </div>
         </div>
-        <div class="row w-75 mb-3 align-items-center">
-            <div class="col-2">
-                <label for="qualifications" class="form-label">Subject Qualifications</label>
+        <div class="" id="lecturerOptions">
+            <div class="row w-75 mb-3 align-items-center">
+                <div class="col-2">
+                    <label for="qualifications" class="form-label">Subject Qualifications</label>
+                </div>
+                <div class="col-4">
+                    <select class="selectpicker w-100 border rounded border-primary" id="qualifications" name="qualifications[]" multiple>
+                        @foreach(SubjectController::getSubjects() as $subject)
+                            <option value="{{ $subject->id }}" @if(isset($user) && $user->qualifications()->get()->contains('id', $subject->id)) selected @endif>[{{ $subject->code }}] {{ $subject->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="col-2">
+                    <label for="color" class="form-label">Schedule Colour</label>
+                </div>
+                <div class="col-4">
+                    <input type="color" name="color" class="form-control form-control-color border-primary" id="color" value="{{ $user->color ?? "#ffffff" }}" title="Assign the colour to be used in the schedule for this user" />
+                </div>
             </div>
-            <div class="col-4">
-                <select class="selectpicker w-100 border rounded border-primary" id="qualifications" name="qualifications[]" multiple>
-                    @foreach(SubjectController::getSubjects() as $subject)
-                        <option value="{{ $subject->id }}" @if(isset($user) && $user->qualifications()->get()->contains('id', $subject->id)) selected @endif>[{{ $subject->code }}] {{ $subject->name }}</option>
-                    @endforeach
-                </select>
-            </div>
-            <div class="col-2">
-                <label for="color" class="form-label">Schedule Colour</label>
-            </div>
-            <div class="col-4">
-                <input type="color" name="color" class="form-control form-control-color border-primary" id="color" value="{{ $user->color ?? "#ffffff" }}" title="Assign the colour to be used in the schedule for this user" />
-            </div>
-        </div>
-        <div class="row w-75 mb-3 align-items-center">
-            <div class="col-2">
-                <label for="load" class="form-label">Subject Load</label>
-            </div>
-            <div class="col-10">
-                <input type="range" name="maxLoad" class="w-75" id="load" min="0.1" max="1" value="{{ $user->maxLoad ?? 0.8 }}" step="0.1" oninput="setLoadValue(this.value)" />
-                <span id="lblLoad"></span>%
+            <div class="row w-75 mb-3 align-items-center">
+                <div class="col-2">
+                    <label for="load" class="form-label">Subject Load</label>
+                </div>
+                <div class="col-10">
+                    <input type="range" name="maxLoad" class="w-75" id="load" min="0.1" max="1" value="{{ $user->maxLoad ?? 0.8 }}" step="0.1" oninput="setLoadValue(this.value)" />
+                    <span id="lblLoad"></span>%
+                </div>
             </div>
         </div>
         <div class="w-75 px-2">
@@ -99,9 +100,20 @@
     </form>
 </div>
 <script type="text/javascript">
+    let userType = document.getElementById("userType");
     let loadVal = document.getElementById("load").value;
-    document.getElementById("lblLoad").onload(setLoadValue(loadVal));
 
+    userType.onchange = hideLecturerOptions;
+    document.getElementById("userType").addEventListener('load', hideLecturerOptions());
+    document.getElementById("lblLoad").addEventListener('load', setLoadValue(loadVal));
+
+    function hideLecturerOptions(){
+        if(userType.value != 0){
+            document.getElementById("lecturerOptions").classList.add("collapse");
+        } else {
+            document.getElementById("lecturerOptions").classList.remove("collapse");
+        }
+    }
     function setLoadValue(loadVal){
         document.getElementById("lblLoad").innerHTML = loadVal * 100;
     }
