3f16e6cfe2fd09388c84986dd316533c183ad724
matt <pricem1990@gmail.com>
2023-01-09 16:04:45 +1000

    ca2-88: AssignLecturer - Fix to allow Unassigned instances to be updated while remaining Unassigned

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -68,7 +68,7 @@ class ScheduleController extends Controller
             $lecturer = User::find($_POST['lecturer']);
         }
 
-        $model->user_id = $lecturer->id;
+        $model->user_id = $lecturer->id ?? null;
         $model->load = $_POST['load'];
         $model->save();
 
